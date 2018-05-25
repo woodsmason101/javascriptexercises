78546989d88a678fcc5698cb0fff38b38f21e13a
woodsmason101 <39624327+woodsmason101@users.noreply.github.com>
2018-05-25 08:34:48 -0500

    Create js5.js

---
+++ js5.js
@@ -0,0 +1,8 @@
+function computeAverageLengthOfWords(word1, word2) {
+  // your code here
+var hello=word1.length + word2.length;
+var pop=hello/2;
+return pop;
+}
+var output = computeAverageLengthOfWords('code', 'programs');
+console.log(output);
